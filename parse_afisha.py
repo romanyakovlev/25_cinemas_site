cbf4a3d186835a01e91b8caac6b65bafabb267ba
romanyakovlev <yakovlevroman8@gmail.com>
2017-02-01 02:52:11 +0700

    add grid system/parse json

--- parse_afisha.py
+++ parse_afisha.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 def fetch_afisha_page():
     request = requests.get('http://www.afisha.ru/krasnoyarsk/schedule_cinema/')
@@ -9,19 +10,29 @@ def fetch_afisha_page():
 def parse_afisha_list(raw_html):
     soup_data =  BeautifulSoup(raw_html, 'html.parser')
     movies_list = soup_data.find_all(class_="object")
-    movies_info = [[movie.find(class_="m-disp-table").a.text, movie.find(class_="m-disp-table").a['href'][2:],
+    movies_info = [[movie.find(class_="m-disp-table").a['href'][2:],
                  len(movie.find_all(class_="b-td-item"))] for movie in movies_list]
     return movies_info
 
 
 def get_movies_info():
     movies_info = parse_afisha_list(fetch_afisha_page())
-    movies_img_links = []
-    for _, (name, link, cinema_quantity) in enumerate(movies_info):
-        request = requests.get('http://'+link+'photo/').content
-        soup_data = BeautifulSoup(request,'html.parser')
-        try:
-            movies_info[_].append(soup_data.find(class_='current-photo').img['src'])
-        except:
-            movies_info[_].append(None)
+    for _, (link, cinema_quantity) in enumerate(movies_info):
+        content = requests.get('http://'+link).content
+        soup_data =  BeautifulSoup(content, 'html.parser')
+        cinema_json = soup_data.find_all(attrs={"type":"application/ld+json"})
+        json_1_text = soup_data.find(attrs={"type":"application/ld+json"}).text
+        json_1 = json.loads(json_1_text)
+        movies_info[_].append(json_1)
+        movies_info[_].append(_)
     return movies_info
+
+if __name__ == '__main__':
+    content = requests.get('http://www.afisha.ru/movie/228065/').content
+    soup_data =  BeautifulSoup(content, 'html.parser')
+    cinema_json = soup_data.find_all(attrs={"type":"application/ld+json"})
+    json_1_text = soup_data.find(attrs={"type":"application/ld+json"}).text
+    json_1 = json.loads(json_1_text)
+
+    for y, x in enumerate(get_movies_info()):
+        print(y, x[-1]['aggregateRating']['ratingValue'], x[-1]['name'])
--- server.py
+++ server.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template
 from parse_afisha import get_movies_info
 
 app = Flask(__name__)
+app.config.update(
+    TEMPLATES_AUTO_RELOAD=True,
+)
 movies_info = get_movies_info()
+
+def grouped(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
 
 @app.route('/')
 def films_list():
 
-    return render_template('films_list.html', movies_info=movies_info)
+    return render_template('films_list.html', movies_info=movies_info[:25])
 
 if __name__ == "__main__":
     app.run()
