d994e757732c73e33c988aeac44cd6c59f564c61
romanyakovlev <yakovlevroman8@gmail.com>
2017-05-01 23:17:27 +0700

    add ajax one by one load cinema item

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, redirect, request
-from parse_afisha import get_movies_info
+from parse_afisha import get_movies_info, parse_afisha_list, fetch_afisha_page, get_movie_info
 from werkzeug.contrib.cache import SimpleCache
 import os
 
@@ -9,8 +9,10 @@ app.config.update(
     TEMPLATES_AUTO_RELOAD=True,
 )
 
+afisha_arr = parse_afisha_list(fetch_afisha_page())
 
-def get_movies_info_cache(get_cache=False):
+
+'''def get_movies_info_cache(get_cache=False):
     movies_info = cache.get('movies-info')
     if movies_info is None:
         movies_info = get_movies_info()
@@ -19,15 +21,36 @@ def get_movies_info_cache(get_cache=False):
     if get_cache is False:
         return jsonify({"response": "ok"})
     else:
-        return movies_info
+        return movies_info'''
 
+def create_cache(json_dict):
+    movies_info = cache.get('movies-info')
+    if movies_info is None:
+        movies_info = []
+
+    if json_dict not in movies_info:
+        movies_info.append(json_dict)
+    twelve_hours_timeout = 12 * 60 * 60
+    cache.set('movies-info', movies_info, timeout=twelve_hours_timeout)
+    return json_dict
 
 @app.route('/')
 def movies_list():
     movies_info = cache.get('movies-info')
-    if movies_info is None:
-        return redirect('/make_cache')
-    return render_template('index_template.html', movies_info=movies_info)
+    if movies_info is not None:
+        if len(movies_info) is 10:
+            return render_template('index_template.html', movies_info=movies_info)
+    return render_template('create_cache_template.html', movies_info=afisha_arr)
+
+
+@app.route('/get_movie_info')
+def movies_list1():
+    keys_set = {'text', 'url', 'name', 'aggregateRating', 'description',
+                'image', 'alternativeHeadline', 'genre'}
+    movie_id  = int(request.args.get('id'))
+    json_arr = get_movie_info(afisha_arr[movie_id]['href'], movie_id, keys_set)
+    json_arr = create_cache(json_arr)
+    return jsonify(json_arr)
 
 
 @app.route('/make_cache', methods=['GET', 'POST'])
@@ -50,5 +73,5 @@ def movies_list_json():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host='0.0.0.0', port=port)
